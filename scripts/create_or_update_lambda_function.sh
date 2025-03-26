d5a330ce32a811fed6d28411cc4e73239203949f
Manas Goyal <manasgoyal@KSH-PC71.local>
2025-03-26 13:34:29 +0900

    testing by setting the api region.

--- scripts/create_or_update_lambda_function.sh
+++ scripts/create_or_update_lambda_function.sh
@@ -1,17 +1,14 @@
 echo "This is the image tag to be used: $SHA"
-echo "SHA: $SHA"
-echo "ACCOUNT_ID: $ACCOUNT_ID"
-echo "REGION_ID: $REGION_ID"
 
 if aws lambda get-function --function-name healthcheck-lambda; then
   echo "Updating Lambda function code..."
   aws lambda update-function-code --function-name healthcheck-lambda \
-    --image-uri ${ACCOUNT_ID}.dkr.ecr.${REGION_ID}.amazonaws.com/lambda-from-container-image:$SHA
+    --image-uri ${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/lambda-from-container-image:$SHA
 else
   echo "Creating new Lambda function 'healthcheck-lambda'..."
   aws lambda create-function --function-name healthcheck-lambda \
     --package-type Image \
-    --code ImageUri=${ACCOUNT_ID}.dkr.ecr.${REGION_ID}.amazonaws.com/lambda-from-container-image:$SHA \
+    --code ImageUri=${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/lambda-from-container-image:$SHA \
     --role arn:aws:iam::${ACCOUNT_ID}:role/Roleforhealthchecklambdaecsupdate \
-    --region ${REGION_ID}
+    --region ap-northeast-1
 fi
