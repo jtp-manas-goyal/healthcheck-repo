dd8aa050ee295228cff94bb3442fe666624034f1
Manas Goyal <manasgoyal@KSH-PC71.local>
2025-03-26 14:02:25 +0900

    commting based on changing the file.

--- scripts/create_or_update_lambda_function.sh
+++ scripts/create_or_update_lambda_function.sh
@@ -1,14 +1,14 @@
-echo "This is the image tag to be used: $SHA"
+echo "This is the image tag to be used:"
 
 if aws lambda get-function --function-name healthcheck-lambda; then
   echo "Updating Lambda function code..."
   aws lambda update-function-code --function-name healthcheck-lambda \
-    --image-uri ${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/lambda-from-container-image:$SHA
+    --image-uri ${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/healthcheck-playwright-repo:latest
 else
   echo "Creating new Lambda function 'healthcheck-lambda'..."
   aws lambda create-function --function-name healthcheck-lambda \
     --package-type Image \
-    --code ImageUri=${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/lambda-from-container-image:$SHA \
+    --code ImageUri=${ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/healthcheck-playwright-repo:latest \
     --role arn:aws:iam::${ACCOUNT_ID}:role/Roleforhealthchecklambdaecsupdate \
     --region ap-northeast-1
 fi
